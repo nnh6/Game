2b81113a6a05cebbc88ecfdbc38ea3923cabbdfa
Justin jud44 <jud44@pitt.edu>
2022-06-09 16:41:20 -0400

    added credits

--- MinerPitfall/target/debug/MinerPitfall.d
+++ MinerPitfall/target/debug/MinerPitfall.d
@@ -1,1 +1,1 @@
-/mnt/c/Users/Matt/Desktop/game/MinerPitfall/target/debug/MinerPitfall: /mnt/c/Users/Matt/Desktop/game/MinerPitfall/src/main.rs
+C:\Users\Justin\desktop\video\ game\ design\Game\MinerPitfall\target\debug\MinerPitfall.exe: C:\Users\Justin\desktop\video\ game\ design\Game\MinerPitfall\src\main.rs
