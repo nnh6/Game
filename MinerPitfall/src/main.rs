089b35d58e1abde6b2ad8fb94a898f85630187ba
trezzamatt <58269391+trezzamatt@users.noreply.github.com>
2022-06-14 11:05:44 -0400

    added rest of slides + jpg to png

--- MinerPitfall/src/main.rs
+++ MinerPitfall/src/main.rs
@@ -22,28 +22,73 @@ fn main() {
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+	//timings: Justin 3, Nara 6, Yinuo 9, Lucas 12, Landin 15, Grant 18, Matt 21
 	commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 	commands
 		.spawn_bundle(SpriteBundle {
 			texture: asset_server.load("monke.png"),
 			..default()
-		});
+		}); 
 	commands
 		.spawn_bundle(SpriteBundle {
 			texture: asset_server.load("justinCredits.png"),
-			transform: Transform::from_xyz(0., 0., -1.),
+			transform: Transform::from_xyz(0., 0., -7.),
 			..default()
 		})
 		.insert(PopupTimer(Timer::from_seconds(3., false)));
+	info!("Hello Justin!");
+	//Nara
+	commands
+	.spawn_bundle(SpriteBundle {
+		texture: asset_server.load("NaraEndCredit.png"),
+		transform: Transform::from_xyz(0., 0., -6.),
+		..default()
+	})
+	.insert(PopupTimer(Timer::from_seconds(6., false)));
+	info!("Hello Nara!");
+	commands
+	.spawn_bundle(SpriteBundle {
+		texture: asset_server.load("yinuo-credit r.png"),
+		transform: Transform::from_xyz(0., 0., -6.),
+		..default()
+	})
+	.insert(PopupTimer(Timer::from_seconds(6., false)));
+	info!("Hello Yinuo!");
+	//Landin
 	commands
 		.spawn_bundle(SpriteBundle {
-			texture: asset_server.load("landincredits.png"),
-			transform: Transform::from_xyz(0., 0., -1.),
+			texture: asset_server.load("lrm88-credit-slide_LI.png"),
+			transform: Transform::from_xyz(0., 0., -3.),
 			..default()
 		})
 		.insert(PopupTimer(Timer::from_seconds(15., false)));
+	info!("Hello Lucas!");
+	commands
+		.spawn_bundle(SpriteBundle {
+			texture: asset_server.load("landin-credits.png"),
+			transform: Transform::from_xyz(0., 0., -3.),
+			..default()
+		})
+		.insert(PopupTimer(Timer::from_seconds(15., false)));
+	info!("Hello Landin!");
+	commands
+		.spawn_bundle(SpriteBundle {
+			texture: asset_server.load("Grant-Credit.png"),
+			transform: Transform::from_xyz(0., 0., -3.),
+			..default()
+		})
+		.insert(PopupTimer(Timer::from_seconds(15., false)));
+	info!("Hello Grant!");
+	commands
+		.spawn_bundle(SpriteBundle {
+			texture: asset_server.load("trezza-credit.png"),
+			transform: Transform::from_xyz(0., 0., -3.),
+			..default()
+		})
+		.insert(PopupTimer(Timer::from_seconds(15., false)));
+	info!("Hello Matt!");
+	
 
-	info!("Hello world!");
 }
 
 fn show_popup(
@@ -53,8 +98,8 @@ fn show_popup(
 	for (mut timer, mut transform) in popup.iter_mut() {
 		timer.tick(time.delta());
 		if timer.just_finished() {
-			transform.translation.z = 2.;
-			info!("Actually is Linux!");
+			transform.translation.z = 7.;
+			info!("End Credits!");
 		}
 	}
 }
